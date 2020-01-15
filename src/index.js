4da39947e4223165f692b8002bf1059f7a53d5e7
Rafael Borges <rafaelfborges@gmail.com>
2020-01-14 21:37:38 -0300

    feat: Setting up .env file and mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
@@ -8,4 +16,4 @@ app.get('/', (request, response) => {
     return response.json({ message: 'Hello Omnistack' });
 });
 
-app.listen(3333);
+app.listen(process.env.PORT);
