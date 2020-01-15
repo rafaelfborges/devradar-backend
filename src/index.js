b2bf601fabb99b04afc291a339c15dbe52b9f57a
Rafael Borges <rafaelfborges@gmail.com>
2020-01-14 21:28:19 -0300

    feat: Setting up nodemon and mongoose dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+    return response.json({ message: 'Hello Omnistack' });
 });
 
 app.listen(3333);
