44b4f6a58f3e0ec5937dd17a7ee6a99b13628b7c
Rafael Borges <rafaelfborges@gmail.com>
2020-01-17 10:46:34 -0300

    feat: Setting up socket.io

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(process.env.MONGO_URL_CONNECTION, {
   useNewUrlParser: true,
@@ -16,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
