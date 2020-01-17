5bc216def1a2545fbf91b846879260fa8b3084b1
Rafael Borges <rafaelfborges@gmail.com>
2020-01-17 05:45:35 -0300

    fix: Script start on package.json and setting up Cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL_CONNECTION, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
